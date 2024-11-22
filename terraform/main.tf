0074e3109cdd8395954a3a955f96e59af0ee24a9
Matthew Adams <42654445+MattyAddy@users.noreply.github.com>
2024-11-22 17:58:28 -0500

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.9.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project = var.project
+  region  = var.region
+}
+
+resource "google_storage_bucket" "book_data_lake" {
+  name          = var.gcs_bucket
+  location      = var.location
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "goodreads_db" {
+  dataset_id = var.big_query_dataset
+  location   = var.location
+}
