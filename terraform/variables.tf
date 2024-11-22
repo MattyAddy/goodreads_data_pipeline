dace052ee0c8476c14e4618b9608af4f8b037f0a
Matthew Adams <42654445+MattyAddy@users.noreply.github.com>
2024-11-22 17:58:53 -0500

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "project" {
+  description = "Project Title"
+  default = "goodreads-madams"
+}
+
+variable "credentials" {
+  type        = string
+  default     = "/home/madams/.google/credentials/google_credentials.json"
+  description = "GCP Credentials"
+}
+
+variable "location" {
+  description = "Location"
+  default = "US"
+}
+
+variable "region" {
+  description = "Region"
+  default = "us-east1"
+}
+
+variable "gcs_bucket" {
+  description = "Goodreads Data Lake"
+  default = "madams-terraform-book-data-lake"
+}
+
+variable "big_query_dataset" {
+  description = "Goodreads Dataset"
+  default = "goodreads_db"
+}
