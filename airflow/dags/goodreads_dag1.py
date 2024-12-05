b9ee07b88330ec2d7c420707da6ebd3640e7a76e
Matthew Adams <42654445+MattyAddy@users.noreply.github.com>
2024-12-05 15:54:55 -0500

    Update goodreads_dag1.py

--- airflow/dags/goodreads_dag1.py
+++ airflow/dags/goodreads_dag1.py
@@ -5,27 +5,25 @@ from airflow.models.baseoperator import chain
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator, BigQueryCreateExternalTableOperator
 from google.cloud import storage
 
+# DAG variables
 start = pendulum.datetime(2024, 11, 9, tz="UTC")
 airflow_file_path = os.environ.get("AIRFLOW_HOME","/opt/airflow/")
 
-# Directory variables
 execution_date_string = "{{ data_interval_start.strftime('%Y%m%d') }}"
 year_folder = "{{ data_interval_start.strftime('%Y') }}"
 month_folder = "{{ data_interval_start.strftime('%m') }}"
 parquet_file = f"goodreads_{execution_date_string}.parquet"
+blob_name = f"madams-terraform-book-data-lake/raw_data/{year_folder}/{month_folder}"
+schema = "goodreads_db"
+external_table = "ext_book_bronze_stg"
+table = "book_bronze_stg"
 
-# File names
 source_file_name = f"{airflow_file_path}/data/{parquet_file}"
 destination_blob_name = f"raw_data/{year_folder}/{month_folder}/{parquet_file}"
 
-# Google environment variables
 GCP_PROJECT = os.environ.get("GCP_PROJECT_ID")
 GCS_BUCKET = os.environ.get("GCP_GCS_BUCKET")
 GCP_DATASET = os.environ.get("GCP_DATASET")
-#GCP_CONNECTION = os.environ.get("AIRFLOW_CONN_GOOGLE_CLOUD_DEFAULT")
-
-external_table = "full_external_stg"
-blob_name = f"madams-terraform-book-data-lake/raw_data/{year_folder}/{month_folder}"
 
 # Taskflow DAG
 @dag(
@@ -37,8 +35,7 @@ blob_name = f"madams-terraform-book-data-lake/raw_data/{year_folder}/{month_fold
 def goodreads_etl_taskflow():
     @task.bash
     def goodreads_extract_data():
-        # return f"python {airflow_file_path}/python_scripts/test_scrape.py > {source_file_name}"
-        return f"python {airflow_file_path}/python_scripts/test_scrape.py"
+        return f"python {airflow_file_path}/python_scripts/goodreads_scrape.py"
     @task
     def goodreads_load_data(bucket,source_file,destination_blob):
         storage_client = storage.Client()
@@ -65,16 +62,8 @@ def goodreads_etl_taskflow():
     }   
     )
 
-    # create_insert_external_table = BigQueryCreateExternalTableOperator(
-    #     task_id = "create_external_table",
-    #     destination_project_dataset_table = f"{GCP_DATASET}.{external_table}",
-    #     bucket = GCS_BUCKET,
-    #     #source_objects = [f"{blob_name}/goodreads*.parquet"],
-    #     source_objects = [f"{blob_name}/goodreads_20241118"],
-    #     )
-
     create_insert_external_table = BigQueryCreateExternalTableOperator(
-        task_id="create_external_table",
+        task_id="create_external_job",
         table_resource = {
             "tableReference": {
                 "projectId": GCP_PROJECT,
@@ -83,7 +72,7 @@ def goodreads_etl_taskflow():
             },
             "externalDataConfiguration": {
                 "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{blob_name}/goodreads*.parquet"],
+                "sourceUris": [f"gs://{blob_name}/{parquet_file}"]
             },
         },
     )
@@ -100,6 +89,7 @@ def goodreads_etl_taskflow():
             "project_id": GCP_PROJECT,
             "dataset_name": GCP_DATASET,
             "external_table_name": external_table,
+            "table_name": table,
         }   
         )
 
@@ -108,13 +98,6 @@ def goodreads_etl_taskflow():
     goodreads_load = goodreads_load_data(GCS_BUCKET,source_file_name,destination_blob_name)
     remove_local_file = remove_local_file_data()
 
-    goodreads_extract >> goodreads_load >> remove_local_file  >> delete_external_table >> create_insert_external_table
-
-    # Establish dependencies
-    # chain(
-    #     goodreads_extract_data(),
-    #     goodreads_load_data(GCS_BUCKET,source_file_name,destination_blob_name),
-    #     remove_local_file_data()
-    #     )
+    goodreads_extract >> goodreads_load >> remove_local_file  >> delete_external_table >> create_insert_external_table >> internal_table_insert
 
 goodreads_etl_taskflow()
