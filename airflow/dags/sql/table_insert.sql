b1fb71798738f68d9c81df358c2f065ef9cbdb30
Matthew Adams <42654445+MattyAddy@users.noreply.github.com>
2024-12-05 15:54:26 -0500

    Create table_insert.sql

---
+++ airflow/dags/sql/table_insert.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS {{ params.dataset_name }}.{{ params.table_name }} (
+        Title STRING, 
+        Author STRING,
+        Genre STRING,
+        NumberOfPages STRING,
+        PublishDate STRING,
+        RatingCount STRING,
+        AverageRating STRING,
+        ReviewCount STRING,
+        ISBN STRING
+);
+ 
+TRUNCATE TABLE {{ params.dataset_name }}.{{ params.table_name }};
+
+MERGE {{ params.dataset_name }}.{{ params.table_name }} t
+USING {{ params.dataset_name }}.{{ params.external_table_name }} s
+ON t.Title = s.Title
+WHEN NOT MATCHED THEN INSERT ROW;
