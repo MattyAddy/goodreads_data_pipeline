5f4ff11abc5a08b63855aa0c7dd0d5678f1aef52
Matthew Adams <matt.adams9898@gmail.com>
2024-12-11 23:01:52 +0000

    adjust back

--- dbt/macros/top_shelf.sql
+++ dbt/macros/top_shelf.sql
@@ -4,8 +4,8 @@
         when averagerating >= 4.5
             and ratingcount >= 100000
             and reviewcount >= 10000
-        then TRUE
-        else FALSE
+        then 1
+        else 0
     end
 
 {%- endmacro %}
